dd7bc9934e0ab0caaff9b4bfdf59f330a18dbfc2
Catevika <dominique.bello@outlook.fr>
2022-03-08 09:20:23 +0100

    Login completed

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -2,6 +2,7 @@ import type { ActionFunction, LinksFunction } from 'remix';
 import { useActionData, json, Link, useSearchParams } from 'remix';
 
 import { db } from '~/utils/db.server';
+import { createUserSession, login } from '~/utils/session.server';
 import stylesUrl from '~/styles/login.css';
 
 export const links: LinksFunction = () => {
@@ -62,14 +63,20 @@ export const action: ActionFunction = async ({ request }) => {
 
 	switch (loginType) {
 		case 'login': {
+			const user = await login({ username, password });
+			console.log({ user });
+			if (!user) {
+				return badRequest({
+					fields,
+					formError: 'Username/Password combination is incorrect'
+				});
+			}
 			// login to get the user
 			// if there's no user, return the fields and a formError
 			// if there is a user, create their session and redirect to /jokes
-			return badRequest({
-				fields,
-				formError: 'Not implemented'
-			});
+			return createUserSession(user.id, redirectTo);
 		}
+
 		case 'register': {
 			const userExists = await db.user.findFirst({
 				// to get only the user id, not all the user data:
@@ -92,7 +99,7 @@ export const action: ActionFunction = async ({ request }) => {
 		default: {
 			return badRequest({
 				fields,
-				formError: `Login type invalid`
+				formError: 'Login type invalid'
 			});
 		}
 	}
@@ -141,6 +148,7 @@ export default function Login() {
 							type='text'
 							id='username-input'
 							name='username'
+							autoComplete='username'
 							defaultValue={actionData?.fields?.username}
 							aria-invalid={Boolean(actionData?.fieldErrors?.username)}
 							aria-errormessage={
@@ -162,6 +170,7 @@ export default function Login() {
 						<input
 							id='password-input'
 							name='password'
+							autoComplete='current-password'
 							defaultValue={actionData?.fields?.password}
 							type='password'
 							aria-invalid={
